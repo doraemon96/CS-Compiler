9b1823d12012148b3a65566127c8b92bb53e659a
Martin <martinceresa@gmail.com>
2018-11-07 13:45:41 -0300

    comentarios en TigerInterp

--- src/TigerInterp.hs
+++ src/TigerInterp.hs
@@ -32,15 +32,17 @@ data Dato
     | DInt Int
     deriving (Show)
 
+-- | Funcinoes auxiliares de proyección con errores significativos
 getInt :: Dato -> Int
+getStr :: Dato -> Symbol
+getFBody :: Dato -> ([Access], [Stm])
+
 getInt (DInt i) = i
 getInt _        = error "NOT AN INT"
 
-getStr :: Dato -> Symbol
 getStr (Str s) = s
 getStr _       = error "NOT A Symbol?"
 
-getFBody :: Dato -> ([Access], [Stm])
 getFBody (FBody sts) = sts
 getFBody _           = error "NOT A FUN"
 
@@ -59,96 +61,168 @@ data CPU = CPU
 
 type RC = State CPU
 
+-- | Función para chequear si es una función externa.
 extCall :: Label -> Bool
 extCall l = all ((== l) . pack) ["print", "flush", "getchar"]
 
+-- | Llamada externa a print.
+-- Dada una dirección de memoria |i : Int| buscamos a que
+-- label | mblab : Label | hace referencias y luego buscamos la
+-- cadena de caracteres | mbstr : String | y lo concatenamos en
+-- output (que sería la salida estandard)
 printExec :: Int -> RC Int
-printExec i = trace (show i) $ do
+printExec i = trace ("Llamada a print con argumento: " ++ show i) $ do
   env <- get
   let mblab = wat env ! i
   let mbstr = dat env ! getStr mblab
   put (env { output = output env ++ [getStr mbstr] })
   return 1
 
+-- | Dispatcher de funciones externas.
 extDispatcher :: Label -> [Int] -> RC Int
 extDispatcher "print" (x : _) = printExec x
 
+-- | |compute| ejecuta las operaciones aritmeticas básicas.
 compute :: BOp -> Int -> Int -> Int
 compute Plus = (+)
 compute _    = error "TODO"
 
+-- | |compare| ejecuta las operaciones de relación entre enteros.
 compare :: Relop -> Int -> Int -> Bool
 compare EQ = (==)
 compare _  = error "TODO"
 
 -- | Exp :: TInt
+-- Ejecución de expresiones, son instrucciones que retornan un entero.
 iexp :: Exp -> RC Int
+-- | Una expresión |Const i| retorna a |i|
 iexp (Const i     ) = return i
+-- | |Name l| representa lo que tenga asignado la label |l|, es decir, hay que buscarlo en memoria.
 iexp (Name  n     ) = trace ("NAME") $ get >>= \e -> return $ getInt (dat e ! n)
+-- | Devolvemos lo que tenga el temporario.
 iexp (Temp  t     ) = get >>= \e -> return $ mem e ! t
+-- | Computamos la operación |op|, viendo que valor toman los argumentos.
 iexp (Binop op x y) = do
+  -- Evaluamos a |x|
   x' <- iexp x
+  -- Evaluamos a |y|
   y' <- iexp y
+  -- Computamos op con |x'| e |y'|
   return $ compute op x' y'
+-- | Básicamente desreferenciamos a lo que apunte |e|
 iexp (Mem e) = do
   e'  <- iexp e
   env <- get
   return $ getInt $ wat env ! e'
+-- | LLamada a función |f| con argumentos |es|.
+-- Esto no está implementado totalmente, y posiblemente tampoco correctamente.
 iexp (Call (Name f) es) = do
+  -- Evaluamos cada uno de los argumentos.
   es'  <- mapM iexp es
   dats <- gets dat
+  -- Chequeamos si es externa
   if (extCall f)
+    -- En el caso que sea llamamos al dispatcher.
     then extDispatcher f es'
-  -- Acá hay que conectar los argumentos con el body de la función
     else do
+    -- En el caso que no sea externa, tendremos que hacer un poco más de trabajo.
+    -- Esto depende de varias cosas, y lo que falta conectar es saber cómo se conectan
+    -- los argumentos desde el punto de vista del llamante y del llamado. Acá se debería
+    -- definir correctamente la convención de llamada.
+    -- TODO: completar
+    -- Buscamos la info de |f| cargada en la CPU. Esto nos da un |acc| y el |body|.
       let (acc, body) = getFBody $ dats ! f
+      -- Deberíamos preparar bien la info de los argumentos, los access de estos
+      -- con los argumentos reales que están en |es'|.
+      ----------------------------------------
+      -- WARNING! Seguramente hay que mejorar esto.
       mapM_ step
         $ zipWith (\a i -> Move (TigerFrame.exp a 0) (Mem (Const i))) acc es'
+      ----------------------------------------
+      -- TODO: Ejecutar el main?
+      -- Buscar el resultado en rv y devolverlo.
       mems <- gets mem
       return $ mems ! rv
+-- En ppio no puede pasar otra cosa. A menos que estemos en un leng funcional ;)
 iexp (Call _ _) = error "Puede pasar?"
+-- | |Eseq| es la ejecución secuencial de los pasos.
 iexp (Eseq s e) = step s >> iexp e
 
+-- | Cada paso de la máquina es la ejecución de un |Stm| que puede derivar en la
+-- necesidad de continuar ejecutando aún más |[Stm]|
 step :: Stm -> RC [Stm]
+-- Un label no hace nada.
 step (Label _              ) = return []
+-- Ejecutamos primeo a |l| y dsp |r|
 step (Seq  l        r      ) = return [l, r]
 -- | Assm load
 step (Move (Temp t) (Mem m)) = do
+  -- Búscamos que entero representa a |m|
   dir  <- iexp m
+  -- Desreferenciamos esa dirección
   wats <- gets wat
-  modify $ \env -> env { mem = M.insert t (getInt $ wats ! dir) (mem env) }
+  let info = (getInt $ wats ! dir)
+  -- Lo movemos a |t|
+  modify $ \env -> env { mem = M.insert t info (mem env) }
   return []
+-- El casogeneral del |Move| (en el que __no__ tenemos que desreferencias memoria),
+-- es más sencillo.
 step (Move (Temp t) src) = do
+  -- Ejecutamos |src|
   val <- iexp src
+  -- y movemos el resultado a |t|
   modify $ \env -> env { mem = M.insert t val (mem env) }
   return []
 -- | Assm store
+-- Igual que antes hacer un |Move| a una memoria
+-- es lo mismo que hacer un /Store/ en assembler. Y por ende un cambia el mapa que usamos.
 step (Move (Mem t) src) = do
+  -- Búscamos que dirección de memoria representa |t|
   dir <- iexp t
+  -- ejecutamos |src|
   val <- iexp src
+  -- actualizamos la memoria.
   modify $ \env -> env { wat = M.insert dir (DInt val) (wat env) }
   return []
+-- Move en el caso que no sea a memoria.
 step (Move dst src) = do
+  -- Computamos |dst| y |src|
   src' <- iexp src
   dst' <- iexp dst
+  -- actualizamos la memoria.
   modify (\env -> env { wat = M.insert dst' (wat env ! src') (wat env) })
   return []
+-- Ejecutar una expresión tirando el resultado.
 step (ExpS e) = iexp e >> return []
+-- El |Jump| queda sencillo, es simplemente búscar el código a ejecutar, y devolverlo.
 step (Jump _ l) = gets dat >>= \dats -> return $ snd $ getFBody $ dats ! l
+-- |CJump| es un jump condicional, no creo que lo usen pero es fácil de implementar.
 step (CJump bop x y tt ff) = do
   x' <- iexp x
   y' <- iexp y
   return
     $ if compare bop x' y' then [Jump (Const 0) tt] else [Jump (Const 0) ff]
 
+-- | |runPC| es la función que va ejecutando paso a paso los diferentes |Stm|.
+-- la idea es que toma una lista de |Stm| y la va ejecutando paso a paso con |step|,
+-- ahora |step :: Stm -> RC [Stm] |, es decir que un paso puede devolvernos más |Stm|
+-- a ejecutar, y lo único que hacemos es concternarlos, /salvo/ en el caso del |Jump|
+-- que se olvida de lo que tenía que ejecutar después.
 runPc :: [Stm] -> RC ()
 runPc []              = return ()
 runPc (l@Jump{} : _ ) = step l >>= runPc
 runPc (x        : xs) = step x >>= \ys -> runPc (ys ++ xs)
 
+--------------------------------------------------------------------------------
+-- Definiciones para que la CPU corra.
+--------------------------------------------------------------------------------
+
+-- | Estado inicial de la CPU.
 emptyCPU :: CPU
 emptyCPU = CPU M.empty M.empty M.empty [] []
 
+-- | Dada una |CPU| y una lista de |[Stm]| ejecutamos dicha lista y obtenemos la
+-- |CPU| resultante.
 runInitial :: CPU -> [Stm] -> CPU
 runInitial cpu prog = execState (runPc prog) cpu
 
@@ -174,7 +248,7 @@ splitLbls (t         : ts) (l, rs) = splitLbls ts (l, t : rs)
 
 -- | Preparamos la CPU para que corra desde un estado inicial.
 loadCPU
-  :: 
+  ::
         -- | Fragmentos de funciones ya definidas. (fuera del main)
      [(Frame, [Stm])]
         -- | Strings.
@@ -214,14 +288,3 @@ loadCPU fs ss tmain
         restLabels
     in
       runInitial inCpu (snd $ head $ restLabels)
-
-test =
-  [ Label "L0"
-  , Move (Temp rv) (Const 1)
-  , Jump (Name "L1") "L1"
-  , Label "L1"
-  , Move (Temp fp) (Const 2)
-  ]
-
-ll :: Label
-ll = "L8"
