b05e2b0f5c8135e7892fa2260747e8cffbbfb437
Nicolas S <soncininicolas@gmail.com>
2019-03-19 15:50:57 -0300

    Starting Maximal Munch

--- src/TigerMunch.hs
+++ src/TigerMunch.hs
@@ -14,21 +14,79 @@ data Instr = OPER { oassem :: String
            | LABEL { lassem :: String
                    , llab   :: Temp.Label }
 
+class (Monad w, TLGenerator w) => Emitter w where
+    emit :: Instr -> w
+
 format :: (Temp.Temp -> String) -> Instr -> String
 format = undefined
 
-munchStm :: Stm -> [Instr]
-munchStm (Seq s1 s2) = (munchStm s1) ++ (munchStm s2)
-munchStm (Move (Mem (Binop Plus e1 (Const i))) e2) = [OPER{ oassem = "sw %s1 , " ++ itos i ++ "(%s0)\n"
-                                                          , osrc = [munchExp e1, munchExp e2]
-                                                          , odst = []
-                                                          , ojmp = Nothing }]
-munchStm () = undefined
-munchStm () = undefined
-munchStm () = undefined
-munchStm (Label l) = [LABEL{lassem = (T.unpack l) ++ ":\n", llab = T.unpack l}]
+munchStm :: (Emitter w) => Stm -> w ()
+munchStm (Seq s1 s2) = munchStm s1 >> munchStm s2
+munchStm (Move (Mem (Binop Plus e1 (Const i))) e2) = do me1 <- munchExp e1
+                                                        me2 <- munchExp e2
+                                                        emit $ OPER{ oassem = "sw %s1, " ++ show i ++ "(%s0)\n" -- STORE M[%s0 + i] <- %s1
+                                                                   , osrc   = [me1, me2]
+                                                                   , odst   = []
+                                                                   , ojmp   = Nothing}
+munchStm (Move (Mem (Binop Plus (Const i) e1)) e2) = do me1 <- munchExp e1
+                                                        me2 <- munchExp e2
+                                                        emit $ OPER{ oassem = "sw %s1, " ++ show i ++ "(%s0)\n" -- STORE M[i + %s0] <- %s1
+                                                                   , osrc   = [me1, me2]
+                                                                   , odst   = []
+                                                                   , ojmp   = Nothing}
+--munchStm (Move (Mem e1 e2)) = do me1 <- munchExp e1
+--                                 me2 <- munchExp e2
+--                                 emit $ OPER{ oassem = "sw %s1, " ++ show i ++ "(%s0)\n" -- MOVE M[%s0] <- M[%s1]
+--                                            , osrc   = [me1, me2]
+--                                            , odst   = []
+--                                            , ojmp   = Nothing}
 
-
-
-munchExp :: Exp -> Temp.Temp
-munchExp x = undefined
+munchExp :: (Emitter w) => Exp -> w Temp.Temp
+munchExp (Mem (Binop Plus e1 (Const i))) = do me1 <- munchExp e1
+                                              t   <- newTemp
+                                              emit $ OPER{ oassem = "lw %d0, " ++ show i ++ "(%s0)\n" -- LOAD %d0 <- M[%s0 + i]
+                                                         , osrc   = [me1]
+                                                         , odst   = [t]
+                                                         , ojmp   = Nothing}
+munchExp (Mem (Binop Plus (Cont i) e1)) = do me1 <- munchExp e1
+                                             t   <- newTemp
+                                             emit $ OPER{ oassem = "lw %d0, " ++ show i ++ "(%s0)\n" -- LOAD %d0 <- M[i + %s0]
+                                                        , osrc   = [me1]
+                                                        , odst   = [t]
+                                                        , ojmp   = Nothing}
+munchExp (Mem (Const i)) = do t <- newTemp
+                              emit $ OPER{ oassem = "lw %d0, " ++ show i ++ "($zero)\n" -- LOAD %d0 <- M[i + 0]
+                                         , osrc   = []
+                                         , odst   = [t]
+                                         , ojmp   = Nothing}
+munchExp (Mem e1) = do me1 <- munchExp e1
+                       t   <- newTemp
+                       emit $ OPER{ oassem = "lw %d0, 0(%s0)" -- LOAD %d0 <- M[%s0 + 0]
+                                  , osrc   = [me1]
+                                  , odst   = [t]
+                                  , ojmp   = Nothing}
+munchExp (Binop Plus e1 (Const i)) = do me1 <- munchExp e1
+                                        t   <- newTemp
+                                        emit $ OPER{ oassem = "addi %d0, %s0, " ++ show i ++ "\n" -- ADDI %d0 <- %s0 + i
+                                                   , osrc   = [me1]
+                                                   , odst   = [t]
+                                                   , ojmp   = Nothing}
+munchExp (Binop Plus (Const i) e1) = do me1 <- munchExp e1
+                                        t   <- newTemp
+                                        emit $ OPER{ oassem = "addi %d0, " ++ show i ++ ", %s0\n" -- ADDI %d0 <- i + %s0
+                                                   , osrc   = [me1]
+                                                   , odst   = [t]
+                                                   , ojmp   = Nothing}
+munchExp (Const i) = do t <- newTemp
+                        emit $ OPER{ oassem = "addi %d0, $zero, " ++ show i ++ "\n" -- ADDI %d0 <- 0 + i
+                                   , osrc   = []
+                                   , odst   = [t]
+                                   , ojmp   = Nothing}
+munchExp (Binop Plus e1 e2) = do me1 <- munchExp e1
+                                me2  <- munchExp e2
+                                t    <- newTemp
+                                emit $ OPER{ oassem = "add %d0, %s0, %s1\n" -- ADD %d0 <- %s0 + %s1
+                                           , osrc   = [me1, me2]
+                                           , odst   = [t]
+                                           , ojmp   = Nothing}
+munchExp (Temp t) = return $ T.unpack t
