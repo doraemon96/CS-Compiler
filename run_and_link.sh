40f4bf9e33e13196075ca9d0f5eb905120c4cbae
Nicolas S <soncininicolas@gmail.com>
2020-11-15 19:26:18 -0300

    Add test cases

--- run_and_link.sh
+++ run_and_link.sh
@@ -1,12 +1,13 @@
 # Script takes in tiger file and returns assembly
 # code ready to run in SPIM
-if (($# != 1)); then
+if (($# < 1)); then
 	echo "Usage: $0 <path/to/filename.tig>"
 	exit 1
 fi
 
 file_tig=$1
 runtime=runtime.s
+out_s=out.s
 
 echo "Running Stack"
 stack build --silent
@@ -17,4 +18,11 @@ stack run -- ${file_tig} -f
 echo "Appending to Runtime"
 file_noext=${file_tig%.tig}
 file_s=${file_noext}.s
-cat runtime.s $file_s > out.s
+cat runtime.s $file_s > $out_s
+
+if (($# == 2)); then
+	echo -e "\n\n\n"
+	echo "Running:" $out_s
+	spim -file $out_s
+	echo -e "\n\n\n"
+fi
